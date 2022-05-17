8a6c1f3382c032dc9e690bf1733a2e2015201fd7
thuggeelya <ilya-fdmdh@mail.ru>
2022-05-17 18:43:10 +0700

    Task 2

--- Task2/src/test/java/org/example/CensoredTextTest.java
+++ Task2/src/test/java/org/example/CensoredTextTest.java
@@ -14,9 +14,9 @@ public class CensoredTextTest
     {
         String text = "Help! I need somebody Help! Not just anybody Help! You know I need someone Help!";
         assertEquals(" censored! I need somebody censored! Not just anybody censored! You know I need someone censored! ", new CensoredText("HELP").censor(' ' + text + ' '));
-        assertEquals(" censored! I need somebody censored! Not just anybody censored! You know I need someone censored! ", new CensoredText("hElP").censor(' ' + text + ' '));
+        assertEquals(" censored! I need somebody censored! Not just anybody censored! You know I need someone censored!", new CensoredText("hElP").censor(' ' + text));
         assertEquals("censored! I need somebody censored! Not just anybody censored! You know I need someone censored!", new CensoredText("help").censor(text));
-        assertEquals("censored! I need somebody censored! Not just anybody censored! You know I need someone censored!", new CensoredText("hElP").censor(text));
         assertEquals(text, new CensoredText("body").censor(text));
+        assertEquals(' ' + text + ' ', new CensoredText("one").censor(' ' + text + ' '));
     }
 }
